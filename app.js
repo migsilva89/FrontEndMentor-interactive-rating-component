db014c436344bf5a73a70a593d59b1afc3336f45
migsilva89 <migdrum@gmail.com>
2022-03-31 22:48:10 +0200

    first tests

--- app.js
+++ app.js
@@ -7,22 +7,23 @@ function btnPressed(result) {
     //FOR LOOP TO GIVE US ALL THE RADIO INPUTS TO [i]
     for (var i = 0; i < radios.length; i++){
 
+        // remove unwanted bg-selected
         document.getElementById("btn" + (i + 1)).classList.remove("bg-selected")
 
+        // LOOP TO MARK RADIO + ADD BG-SELECTED
         if (result == parseInt(radios[i].value)){
             document.getElementById("btn" + (i + 1)).classList.add("bg-selected")
             radios[i].checked = true
             selectedValue = (i + 1)
-
-            // console.log::
-            console.log(`I pressed ${i + 1}`)
-            console.log(selectedValue)
         }
     }
 }
 
+
 function btnSubmit() {
-    const mainForm = document.getElementById("main-form")
+    const mainForm = document.getElementById("main-card")
     mainForm.classList.add('hide')
     resultS.classList.remove('hide')
+    const answer = document.getElementById("answer")
+    answer.innerHTML = selectedValue
 }
